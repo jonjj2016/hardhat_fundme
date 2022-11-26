fbb825892e22a26f05ae49bd2d5311422b2c2465
jonJJ2016 <jonmartirosyan1988@gmail.com>
2022-11-27 03:54:40 +0800

    added mock contract to get exchange rate

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -1,9 +1,9 @@
-const { ethers, network } = require('hardhat')
+const { network } = require('hardhat')
 require('dotenv').config()
 const { networkConfig } = require('../helper-hardhat-config')
 
 module.exports = async ({ deployments, getNamedAccounts }) => {
-  const { deploy, log } = deployments
+  const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
   const chainId = network.config.chainId
   const ethUsdPriceFeedAddress = networkConfig[chainId]
